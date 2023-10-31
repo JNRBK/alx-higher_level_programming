d42eacf9f4defec1d2b12b153647fbec8f5d2488
JNRBK <rafeekmorgan@yahoo.com>
2023-10-31 23:53:35 +0200

    islower

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for a in range(0, 10):
+    for b in range(0, 10):
+        if a >= b:
+            continue
+        elif:
+            print("{:d}{:d}".format(a, b))
+        else:
+            print("{:d}{:d}".format(a, b), end=", ")
