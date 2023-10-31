646fc7592beae01af14c6c4fc7c097b017cbc009
JNRBK <rafeekmorgan@yahoo.com>
2023-11-01 00:00:34 +0200

    islower

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ for a in range(0, 10):
     for b in range(0, 10):
         if a >= b:
             continue
-        elif:
+        elif a == 8 and b == 9:
             print("{:d}{:d}".format(a, b))
         else:
             print("{:d}{:d}".format(a, b), end=", ")
