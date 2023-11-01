233847e6f0e9bbdfe1da076bf9bed7c2608a88c0
JNRBK <rafeekmorgan@yahoo.com>
2023-11-01 04:10:45 +0200

    magic ?

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
-    if a + b == c:
+    if a + b == c or a + b == c + c:
         return (a + b + c) - c
-    if a + b > c + c:
+    if a + b > c:
         return (a * b) - c
