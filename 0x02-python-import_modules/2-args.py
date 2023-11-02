be198be2bdde12c5e900cb779542e969031d913a
JNRBK <rafeekmorgan@yahoo.com>
2023-11-02 21:14:02 +0200

    argc

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    v = len(sys.argv) - 1
+    if v == 0:
+        print("0 arguments.")
+    elif v == 1:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(v))
+
+    for i in range(v):
+        print("{}: {}".format(i, sys.argv[i + 1]))
