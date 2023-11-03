28f2c3114e56b998466eef2f0efb73e59f8fe77d
JNRBK <rafeekmorgan@yahoo.com>
2023-11-03 04:57:10 +0200

    hidden

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,4 +1,5 @@
 #!usr/bin/python3
+import dis
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
@@ -8,3 +9,5 @@ def magic_calculation(a, b):
         return c
     else:
         return sub(a, b)
+
+dis.dis(magic_calculation)
