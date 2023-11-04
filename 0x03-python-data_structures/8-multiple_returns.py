7dbba3187f9b54d08d74818f8b0672b0aef61371
JNRBK <rafeekmorgan@yahoo.com>
2023-11-05 01:05:49 +0200

    len at idx

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == "":
-        sentence = None
+        sentence = None, len(sentence)
     s = len(sentence), sentence[0]
     return s
