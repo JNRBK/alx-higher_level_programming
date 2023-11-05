e4ed879580fc3df8f60c07e03c213d9cf2aecadc
JNRBK <rafeekmorgan@yahoo.com>
2023-11-05 02:18:22 +0200

    switch

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     for idx in my_list:
-        if len(my_list) == "":
+        if my_list == "":
             return None
         my_list.sort()
     return my_list[-1]
