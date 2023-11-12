72aeac2249a1f038294207a2d4e25b6306d5dbcb
JNRBK <rafeekmorgan@yahoo.com>
2023-11-12 21:57:53 +0200

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,34 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None:
+    if not roman_string:
         return 0
+    total = 0
 
-    frst_num = 0
-    scnd_num = 0
+    ro = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
-    for num in roman_string:
-        if num == 'I':
-            v = 1
-        elif num == 'V':
-            v = 5
-        elif num == 'X':
-            v = 10
-        elif num == 'L':
-            v = 50
-        elif num == 'C':
-            v = 100
-        elif num == 'D':
-            v = 500
-        elif num == 'M':
-            v = 1000
+    for n in range(len(roman_string)):
+        if n + 1 < len(roman_string) and \
+                ro[roman_string[n]] < ro[roman_string[n + 1]]:
+            total -= ro[roman_string[n]]
         else:
-            return 0
-
-        if v > scnd_num:
-            frst_num += v - scnd_num * 2
-        else:
-            frst_num += v
-
-        scnd_num = v
-
-    return frst_num
+            total += ro[roman_string[n]]
+    return total
