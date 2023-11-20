ba07e0175127382f662c6a74faeea84e039364fb
JNRBK <rafeekmorgan@yahoo.com>
2023-11-20 20:36:49 +0200

    2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,6 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
                 itr += 1
     except (IndexError, ValueError):
         raise
-    finally:
-        print()
-        return itr
+
+    print()
+    return itr
