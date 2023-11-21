f748c5fe705dd5b41cadcbea5a4c6fae22f4938a
JNRBK <rafeekmorgan@yahoo.com>
2023-11-22 00:52:36 +0200

    3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Square Class with the size'''
+
+
+class Square:
+    """ __init__ method.
+    size: size of square
+    """
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        '''calculate and return area of square'''
+        return self.__size ** 2
