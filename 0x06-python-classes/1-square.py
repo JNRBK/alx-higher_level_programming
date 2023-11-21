5907b52c3a1a9a9b8a0dda727b6db47f206e9dc9
JNRBK <rafeekmorgan@yahoo.com>
2023-11-22 00:11:51 +0200

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Square Class with the size'''
+
+
+class Square:
+    """ init method.
+    size: size of square
+    """
+    def __init__(self, Size):
+        self.Size = Size
