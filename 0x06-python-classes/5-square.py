3faa30f7247a44c54f12e3bf4fe832c7768648a2
JNRBK <rafeekmorgan@yahoo.com>
2023-11-22 03:03:17 +0200

    5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+'''Square Class with the size'''
+
+
+class Square:
+    """ __init__ method.
+    size: size of square
+    """
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    '''property of def size to retrieve it'''
+    @property
+    def size(self):
+        return self.__size
+
+    '''property setter to set it'''
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        '''calculate and return area of square'''
+        return self.__size ** 2
+
+    """
+    my_print print a square shape to stdout with
+    the character "#"
+    """
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for x in range(self.__size):
+                for y in range(self.__size):
+                    print('#', end="")
+                print()
