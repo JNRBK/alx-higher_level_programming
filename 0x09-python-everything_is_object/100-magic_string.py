4d4ac6dd10e054abbdf36854a5e1daa47f4ef725
JNRBK <rafeekmorgan@yahoo.com>
2023-11-29 04:07:27 +0200

    100

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def magic_string():
     """magic_string
-    Write a class LockedClass with no class or object attribute, that prevents the user 
+    Write a class LockedClass with no class or object attribute,
+    that prevents the user
     from dynamically creating new instance attributes,
     except if the new instance attribute
     is called first_name.
