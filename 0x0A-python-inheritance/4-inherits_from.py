d25c0d8d61e1634d7aea6f4a9b20d73dfa3b6c24
JNRBK <rafeekmorgan@yahoo.com>
2023-12-04 23:27:57 +0200

    as

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,9 @@ that inherited from specified class otherwise false
 
 
 def inherits_from(obj, a_class):
+    """
+    Args:
+    @obj: object for class
+    @a_class: class of the object
+    """
     return issubclass(type(obj), a_class) and type(obj) is not a_class
