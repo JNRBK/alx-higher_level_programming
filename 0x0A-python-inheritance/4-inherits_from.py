8ce96d08d419d933e225303005d7b5a70a91cd0a
JNRBK <rafeekmorgan@yahoo.com>
2023-12-04 22:58:10 +0200

    a

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -11,7 +11,6 @@ def inherits_from(obj, a_class):
     Args:
     @obj: object
     @a_class: class of object
-
     Returns: True or False
     """
     return (issubclass(type(obj), a_class) and type(obj) is not a_class)
