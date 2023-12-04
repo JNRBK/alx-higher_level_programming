55841339754d8ea554b42b5983a71adaa711c655
JNRBK <rafeekmorgan@yahoo.com>
2023-12-04 23:34:00 +0200

    as

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+class base geometry
+"""
+
+
+class BaseGeometry:
+    """structure of class"""
+    def area(self):
+        """ area function that raises and exception """
+        raise Exception("area() is not implemented")
