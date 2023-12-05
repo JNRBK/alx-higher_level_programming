0f1adadbb8de2ae5469986f53c15d5fd93cfb495
JNRBK <rafeekmorgan@yahoo.com>
2023-12-06 00:24:38 +0200

    as

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+script that adds all arguments to a python list
+and then save them to a file
+"""
+
+import sys
+
+
+save_to = __import__('5-save_to_json_file').save_to_json_file
+load_from = __import__('6-load_from_json_file').load_from_json_file
+
+
+try:
+    load = load_from("add_item.json")
+except FileNotFoundError:
+    load = []
+
+for arguments in sys.argv[1:]:
+    load.append(arguments)
+
+save_to(load, "add_item.json")
