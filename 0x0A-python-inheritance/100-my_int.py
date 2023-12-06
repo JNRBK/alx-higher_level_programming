4fcf025f5f9bc7102c817fd271e5185bdf6af3fe
JNRBK <rafeekmorgan@yahoo.com>
2023-12-06 20:21:17 +0200

    as

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+class MyInt that inherits from int
+"""
+
+
+class MyInt(int):
+    """
+    Args:
+    @value: value of class
+    """
+    def __init__(self, value):
+        self.__value = value
