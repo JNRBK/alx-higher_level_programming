7c786bd6cd5fbecd037411afd17509101711ed8e
JNRBK <rafeekmorgan@yahoo.com>
2023-12-06 20:50:18 +0200

    a

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -11,3 +11,9 @@ class MyInt(int):
     """
     def __init__(self, value):
         self.__value = value
+
+    def __eq__(self, other):
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        return super().__eq__(other)
