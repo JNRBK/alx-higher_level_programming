8f543e9b53c80f89402e92b04bd3d7c3654a5ed8
JNRBK <rafeekmorgan@yahoo.com>
2023-12-07 03:19:14 +0200

    as

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+Function that returns the dictionary description
+with simple data structure
+"""
+
 
 def class_to_json(obj):
-    
+    """
+    Args:
+    @obj: instance of a class
+    """
+    return vars(obj)
