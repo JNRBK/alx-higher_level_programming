fc8a4bef8cd194a34e52d8950c8df92b19ffa3f5
JNRBK <rafeekmorgan@yahoo.com>
2023-12-13 06:39:09 +0200

    a

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -8,8 +8,30 @@ import json
 class Test_Rectangle(unittest.TestCase):
 
     def test_rectangle(self):
+
         rec = Rectangle(10, 5)
         self.assertEqual(rec.area(), 50)
+
         r1 = Rectangle(1, 2)
         self.assertEqual(r1.width, 1)
         self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 0)
+        self.assertEqual(r1.y, 0)
+        self.assertEqual(r1.id, 2)
+
+        a1 = Rectangle(1, 2, 5)
+        self.assertEqual(a1.width, 1)
+        self.assertEqual(a1.height, 2)
+        self.assertEqual(a1.x, 5)
+        self.assertEqual(a1.y, 0)
+        self.assertEqual(a1.id, 3)
+
+        s1 = Rectangle(1, 2, 5, 4)
+        self.assertEqual(s1.width, 1)
+        self.assertEqual(s1.height, 2)
+        self.assertEqual(s1.x, 5)
+        self.assertEqual(s1.y, 4)
+        self.assertEqual(s1.id, 4)
+
+        with self.assertRaises(TypeError):
+            err = Rectangle("1", 2)
