a6f57118f32d5a39016576a3a599dc129b07913c
JNRBK <rafeekmorgan@yahoo.com>
2023-12-13 03:49:40 +0200

    a

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -3,7 +3,12 @@ from models.base import Base
 from models.square import Square
 
 class base_test(unittest.TestCase):
-    def test_args(self):
+    def test_Base(self):
+        b = Base()
+        self.assertEqual(b.id, 1)
         b1 = Base()
+        b1.id = 1
+        self.assertEqual(b1.id, 1)
         b2 = Base()
-        self.assertEqual(b1.id, b2.id - 1)
+        b3 = Base()
+        self.assertEqual(b2.id + 1, b3.id)
