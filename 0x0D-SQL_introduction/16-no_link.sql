39a99cf7e88a5a07d3d0c3e249552230a7d4160d
JNRBK <rafeekmorgan@yahoo.com>
2024-02-14 21:02:51 +0200

    mod

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 -- list all records of table second_table of DB
 -- hbtn_0c_0 
-SELECT score, COUNT(*) AS number FROM second_table
+SELECT score, name FROM second_table
 WHERE name IS NOT NULL
 GROUP BY score
 ORDER BY score DESC;
