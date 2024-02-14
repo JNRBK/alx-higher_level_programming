de4e3154ac5cd7a05b0a5ee5ffe598c297976e0a
JNRBK <rafeekmorgan@yahoo.com>
2024-02-14 04:56:32 +0200

    MYSQL

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- lists all records with a score >= 10 in table
 --  second_table of the database hbtn
-SELECT score, name FROM second_table WHERE score >= 10;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score;
