6ed295e5555972c5dd335d363aedbbeaf4ac75ec
JNRBK <rafeekmorgan@yahoo.com>
2024-02-15 01:08:39 +0200

    grant

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- create the MYSQL server User user_0d_1
+-- should have all privileges
+-- set password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL privileges ON *.* TO 'user_0d_1'@'localhost';
+SET PASSWORD FOR 'user_0d_1'@'locathost' = 'user_0d_1_pwd';
