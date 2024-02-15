cf4b115bce7a4c38c2656ffb711573f3b5aba547
JNRBK <rafeekmorgan@yahoo.com>
2024-02-15 02:19:58 +0200

    db.table mod

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,7 @@
 -- create the database hbtn_0d_usa and the
 -- and the table states in it
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
-    id INT NOT NULL AUTO_INCREMENT UNIQUE,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY(id)
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
 );
