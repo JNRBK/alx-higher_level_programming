511e65e4ad04a5ca6b7bf06f83eead3dc891b7ba
JNRBK <rafeekmorgan@yahoo.com>
2024-02-15 18:14:36 +0200

    inner join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,4 +2,4 @@
 -- each record should display cities.id - cities.name - states.name
 -- can use only one SELECT
 SELECT cities.id, cities.name, states.name FROM cities
-INNER JOIN states ORDER BY cities.id ASC;
+INNER JOIN states ORDER BY cities.id;
