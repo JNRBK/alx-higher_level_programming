6f3f53ef3999ea982b70b51eeff4d3773ba397ec
JNRBK <rafeekmorgan@yahoo.com>
2024-02-15 18:34:33 +0200

    innerjoinON

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,4 +2,5 @@
 -- each record should display cities.id - cities.name - states.name
 -- can use only one SELECT
 SELECT cities.id, cities.name, states.name FROM cities
-INNER JOIN states ORDER BY cities.id;
+INNER JOIN states ON cities.state_id = states.id
+ORDER BY cities.id;
