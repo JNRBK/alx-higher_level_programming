85d0054e0e06e3b86e1545558fa0ccf84287e364
JNRBK <rafeekmorgan@yahoo.com>
2024-02-15 17:23:47 +0200

    commented

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,6 @@
--- create
+-- create DB hbtn_0d_usa and table cities
+-- column state_id has a foreign key the references to 
+-- id of the states table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
