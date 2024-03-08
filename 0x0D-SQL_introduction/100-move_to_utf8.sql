beb670ca5b7b456d5f01be93f1e93511be499223
JNRBK <rafeekmorgan@yahoo.com>
2024-03-09 01:08:49 +0200

    utf collate

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -8,7 +8,7 @@ ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
 collate utf8mb4_unicode_ci;
 
-ALTER TABLE hbtn_0c_0
+ALTER TABLE first_table
 MODIFY name VARCHAR(256)
 CHARACTER SET utf8mb4
 collate utf8mb4_unicode_ci;
