322a61cc9fb59d9869f2facc8514d000ca5e09f6
JNRBK <rafeekmorgan@yahoo.com>
2024-03-12 03:25:25 +0200

    replace 12 with 89

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 let myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
+  type: 'object',
+  value: 12
+}; console.log(myObject);
 myObject = {
-    type: 'object',
-    value: 89
-  };
-  console.log(myObject);
+  type: 'object',
+  value: 89
+};
+console.log(myObject);
