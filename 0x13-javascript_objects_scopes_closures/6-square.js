db7728c86a26e7760a9d65335be7ee833b9116d7
JNRBK <rafeekmorgan@yahoo.com>
2024-03-13 22:37:50 +0200

    XorC

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const square = require('./5-square');
+
+class Square extends square {
+  charPrint (c) {
+    for (let i = 0; i < this.height; i++) {
+      if (c) {
+        console.log('C'.repeat(this.width));
+      } else {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
