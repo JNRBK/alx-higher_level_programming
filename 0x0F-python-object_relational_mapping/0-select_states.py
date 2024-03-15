5bfe22238d497535a29e804845aaf25e5876204f
JNRBK <rafeekmorgan@yahoo.com>
2024-03-15 03:56:04 +0200

    MySQLdb

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+lists all states from the database hbtn_0e_0_usa
+ARGS:
+    username: name of user in mysql
+    password: password used in mysql
+    database_name: name of the DB
+USAGE:
+    ./0-select_states.py root root hbtn_0e_0_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+db = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=argv[1],
+    passwd=argv[2],
+    db=argv[3]
+    )
+cur = db.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC;")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+cur.close()
+db.close()
---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This script connects to a MySQL database and filters
+the states table based on a given name.
+
+Args:
+    user: The username for the MySQL database.
+    passwd: The password for the MySQL database.
+    db: The name of the MySQL database.
+    do: The name to filter the states table by.
+
+Returns:
+    None
+"""
+import MySQLdb
+from sys import argv
+
+db = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=argv[1],
+    passwd=argv[2],
+    db=argv[3],
+)
+
+cur = db.cursor()
+do = argv[4]
+
+cur.execute('SELECT * FROM states WHERE name = %s', (do,))
+
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
+
+cur.close()
+db.close()
