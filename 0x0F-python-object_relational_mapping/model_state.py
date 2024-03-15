1a1de470c26a7658cba4c93d5497d07f769beb60
JNRBK <rafeekmorgan@yahoo.com>
2024-03-16 01:46:51 +0200

    alchemy

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+python file that contains the class definition of a State and an instance
+ARGS:
+    id - class attribute that represents a column
+    name - class attribute that represents a column
+"""
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+
+conect = 'mysql://root:root@localhost:3306/hbtn_0e_6_usa'
+
+engine = create_engine(conect)
+Base.metadata.create_all(engine)
