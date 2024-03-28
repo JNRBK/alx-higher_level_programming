f95a1c1806464fcc925bff064acc0db017def35e
JNRBK <rafeekmorgan@yahoo.com>
2024-03-29 00:34:02 +0200

    fetch with requess

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+python script that fetches url using package requests
+"""
+
+import requests
+
+if __name__ == "__main__":
+    ftch = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(ftch.text)))
+    print("\t- content: {}".format(ftch.content.decode('utf-8')))
