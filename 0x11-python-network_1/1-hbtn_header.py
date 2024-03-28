70e91fd197e0763ce1e3d9c6e81d619c497dcadd
JNRBK <rafeekmorgan@yahoo.com>
2024-03-28 20:31:19 +0200

    display value of

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,6 +5,6 @@ display value of X-Request-id variable
 """
 import urllib.request
 from sys import argv
-
-with urllib.request.urlopen(argv[1]) as var:
-    print(var.headers.get('X-Request-id'))
+if __name__ == "__main__":
+    with urllib.request.urlopen(argv[1]) as var:
+        print(var.headers.get('X-Request-id'))
