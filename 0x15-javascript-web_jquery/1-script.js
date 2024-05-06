4577e1e300dcea6370c8aab6435e06df1cd6c3bd
JNRBK <rafeekmorgan@yahoo.com>
2024-05-06 21:59:51 +0300

    jquery addClass

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,5 @@
+/* eslint-env jquery */
+/* eslint-disable no-undef */
+$(document).ready(function () {
+  $('header').css('color', '#FF0000');
+});
---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,7 @@
+/* eslint-env jquery */
+/* eslint-disable no-undef */
+$(document).ready(function () {
+  $('#red_header').click(function () {
+    $('header').css('color', '#FF0000');
+  });
+});
