0ef7aabbdccb7ef2b064a9b85959bad7b6796a9e
JNRBK <rafeekmorgan@yahoo.com>
2024-05-07 01:07:51 +0300

    update text

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,7 @@
+/* eslint-env jquery */
+/* eslint-disable no-undef */
+$(document).ready(function () {
+  $('#update_header').click(function () {
+    $('header').text('New Header!!!');
+  });
+});
