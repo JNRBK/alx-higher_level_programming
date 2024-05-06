5cf83a5d4c06078b6b2cbba354059133275f2baf
JNRBK <rafeekmorgan@yahoo.com>
2024-05-06 23:43:25 +0300

    only one class

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,11 @@
+/* eslint-env jquery */
+/* eslint-disable no-undef */
+$(document).ready(function () {
+  $('#toggle_header').click(function () {
+    if ($('header').hasClass('red')) {
+      $('header').removeClass('red').addClass('green');
+    } else {
+      $('header').removeClass('green').addClass('red');
+    }
+  });
+});
